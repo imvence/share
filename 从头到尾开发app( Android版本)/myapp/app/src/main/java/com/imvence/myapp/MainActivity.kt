a81f6cba93489efedde1ebdfe5cd0266af4213b6
imvence <fs750com@aliyun.com>
2020-12-14 18:55:36 +0800

    更新2020年12月14日视频内容

--- 从头到尾开发app( Android版本)/myapp/app/src/main/java/com/imvence/myapp/MainActivity.kt
+++ 从头到尾开发app( Android版本)/myapp/app/src/main/java/com/imvence/myapp/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        navView.itemIconTintList = null //去掉图标的默认背景色
 
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
