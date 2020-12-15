e50b100fd2498f33ebc8d00e2bf9b164defc1bf4
imvence <fs750com@aliyun.com>
2020-12-15 16:16:35 +0800

    更新2020年12月14日视频内容

--- 从头到尾开发app( Android版本)/myapp/app/src/main/java/com/imvence/myapp/MainActivity.kt
+++ 从头到尾开发app( Android版本)/myapp/app/src/main/java/com/imvence/myapp/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.imvence.myapp
 
+import android.os.Build
 import android.os.Bundle
+import android.view.View
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -23,5 +25,22 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_home, R.id.navigation_friends, R.id.navigation_news, R.id.navigation_mine))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        this.changeStatusBarColor(true);
+    }
+    //设置状态栏字体颜色
+    fun changeStatusBarColor(setDark: Boolean) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val decorView: View = window.decorView
+            if (decorView != null) {
+                var vis: Int = decorView.systemUiVisibility
+                vis = if (setDark) {
+                    vis or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+                } else {
+                    vis and View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR.inv()
+                }
+                decorView.systemUiVisibility = vis
+            }
+        }
     }
 }
